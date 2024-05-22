38c2638a94fe98d47fc83086b1062304e4fdcc6e
Agnès Cappello <senga.ds@gmail.com>
2024-05-22 20:09:47 +0200

    delta

--- table2.php
+++ table2.php
@@ -16,3 +16,28 @@ for($i=1;$i<11;$i++){
 };
 
 echo '</table>';
+
+
+echo '</br>';
+
+function racine($a, $b, $c){
+$delta = $b*$b - (4*$a*$c);
+echo 'la valeur de delta est :'. $delta. '</br>';
+if ($a == 0){
+	echo 'equation invalide, on ne peut pas diviser par 0 !';
+	return;
+}
+else {
+	if ($delta == 0 ){
+		echo 'une seule solution : '. -$b/(2*$a);
+	  } 	else if ($delta > 0) {
+		$racineA = (-$b -sqrt($delta))/(2*$a);
+		$racineB = (-$b +sqrt($delta))/(2*$a);
+		echo 'deux solutions : '.$racineA .' et ' .$racineB;
+	  } else if($delta < 0) {
+		echo 'pas de solution';
+	  }
+	}	
+}
+
+racine (1,2,1);
